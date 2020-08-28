1dd64c4bd4899ba1911fb362787f00ea410ac677
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-29 00:21:21 +0300

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { delay, tap } from 'rxjs/operators';
 import { AppService } from './app.service';
 
 @Component({
@@ -7,9 +9,26 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  myString = 'Deneme';
+
+  mySubscribe;
   constructor(
     private appService: AppService
   ) {
+    from(this.myString).pipe(delay(2000), tap(data => {
+      console.log(data);
+    }))
+      .subscribe(data => {
+      console.log(data);
+    })
 
+    this.appService.myAjax.subscribe(data => {
+      console.log(data);
+      });
+  }
+
+  stopSubscribe(): void {
+    this.mySubscribe.unsubscribe();
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { delay, map, mapTo, mergeAll, mergeMap, toArray } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+
+  myAjax = ajax.getJSON('https://jsonplaceholder.typicode.com/users')
+    .pipe(
+      mergeMap((mm: any) => mm = from(mm)),
+      delay(1000)
+    );
 
 constructor() { }
 
