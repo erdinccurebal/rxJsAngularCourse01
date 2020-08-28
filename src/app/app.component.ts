410cc6957f25bcdee7fb493931fa87b68470e871
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-29 02:29:08 +0300

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 import { AppService } from './app.service';
 
 @Component({
@@ -7,9 +8,50 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  myTimer = timer(1, 1000);
+  subscribe;
+  time = 0;
+  endTimer = 0;
+  status;
   constructor(
     private appService: AppService
   ) {
 
   }
+
+  startTimer(): void {
+    this.status = true;
+    if (this.time !== 0) {
+      this.subscribe.unsubscribe();
+      this.subscribe = this.myTimer.subscribe(data => {
+        this.time = data + this.endTimer;
+       });
+    } else {
+      this.subscribe = this.myTimer.subscribe(data => {
+        this.time = data;
+       });
+    }
+
+  }
+
+  stopTimer(): void {
+    this.status = false;
+    this.endTimer = this.time;
+    this.subscribe.unsubscribe();
+  }
+
+  resetAndStartTimer(): void {
+    this.endTimer = 0;
+    this.subscribe.unsubscribe();
+    this.startTimer();
+  }
+
+  resetTimer(): void {
+    this.status = false;
+    this.time = 0;
+    this.subscribe.unsubscribe();
+  }
+
+
 }
