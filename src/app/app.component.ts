872c128db641a9a38bdc64e056b1ddcb7fd10c4a
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 01:18:35 +0300

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,14 @@ export class AppComponent {
   constructor(
     private appService: AppService
   ) {
+    this.appService.myDate.subscribe(data => {
+      console.log('Normal Date: ' + data);
+    });
 
+    this.appService.myTimer.subscribe(() => {
+      this.appService.myDeferDate.subscribe(data => {
+        console.log('Defer Date: ' + data);
+      });
+    });
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { defer, of, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  myTimer = timer(3000);
+  myDate = of(new Date());
+  myDeferDate = defer(() => of(new Date()));
 
 constructor() { }
 
