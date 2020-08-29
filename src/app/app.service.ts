fa4995493b54ea67b5778ad7e4411d406f0688c2
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 02:51:25 +0300

    3rd commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,6 +10,7 @@ import { find, mergeMap } from 'rxjs/operators';
 export class AppService {
   constructor() {}
 
+  // ? Find operatorü tek obje döner ve net aramalar yapılır , tek harf aranmaz, hata mesajı dönmez.
   myFnAjax(): Observable<any> {
     return ajax.getJSON<any>('https://jsonplaceholder.typicode.com/users').pipe(
       mergeMap((mm) => from(mm)),
