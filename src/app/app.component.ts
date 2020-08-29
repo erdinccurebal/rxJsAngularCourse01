fe4c1566cedeeb7f6cf2643fc375dba60b522baf
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-29 22:59:55 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,37 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { AppService } from './app.service';
 import { ChildComponent } from './child/child.component';
-import { fromEvent, interval } from 'rxjs';
+import { from, fromEvent, interval, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild(ChildComponent, {static: false}) childComponentRef: ChildComponent;
+export class AppComponent implements AfterViewInit, OnDestroy {
+  @ViewChild(ChildComponent, { static: false })
+  childComponentRef: ChildComponent;
 
-  constructor(private appService: AppService) {}
+  @ViewChild('myInput')
+  myInput: ElementRef;
+
+  private subscription: Subscription;
+
+  constructor(private appService: AppService) { }
+
   ngAfterViewInit(): void {
+
+    const eventInput = fromEvent<any>(this.myInput.nativeElement, 'keyup')
+      .pipe(
+        map(event => event.target.value),
+        startWith(''),
+        debounceTime(400),
+        distinctUntilChanged()
+    );
+
+    this.subscription = eventInput.subscribe(data => console.log(data));
+
     fromEvent(
       this.childComponentRef.myDateCaller.nativeElement,
       'click'
@@ -21,5 +40,9 @@ export class AppComponent implements AfterViewInit {
         this.childComponentRef.myDate = new Date();
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
