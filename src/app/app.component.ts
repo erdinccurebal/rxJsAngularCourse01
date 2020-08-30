8af15e2c7ed080e0d2a37e9e4090632daeba910d
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-31 00:37:38 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { takeWhile, toArray } from 'rxjs/operators';
-import { from, range } from 'rxjs';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private appService: AppService
-  ) {
-    // ? Verileri şarta bağlı çeker, Baştan Sona sadece ilk verileri çeker;
-    range(1, 10).pipe(
-      takeWhile(data => data < 3)
-    ).subscribe(data => console.log(data));
-
+  constructor(private appService: AppService) {
+    range(1, 10)
+      .pipe(takeWhile((data) => data < 3))
+      .subscribe((data) => console.log(data));
   }
 }
