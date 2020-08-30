32b27ddb10d58d2536e5d581abbb23ae72b719fa
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 20:08:44 +0300

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 import { AppService } from './app.service';
+import { throttle } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private appService: AppService
-  ) {
-
+  constructor(private appService: AppService) {
+    // ? Throttle Operator observable 'dan gelen veriyi belirli bir süre engeller
+    const myInterval = interval(1000);
+    myInterval
+      .pipe(throttle((value) => interval(2000)))
+      .subscribe((data) => console.log(data));
   }
 }
