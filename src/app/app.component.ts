245de5cca2a2355e1f388dd1cb4f54b517227c50
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 19:44:35 +0300

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { from } from 'rxjs';
+import { takeLast } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private appService: AppService
-  ) {
+  myFrom = from([2, 44, 64, 75, 86, 95, 123, 432, 4564]);
 
+  constructor(private appService: AppService) {
+    // ? Parametre olarak int değer alır ve geriye int değer kadar obje çeker. Sondan Başa düzeninde verileri çeker.
+    this.myFrom.pipe(takeLast(5)).subscribe((data) => console.log(data));
   }
 }
