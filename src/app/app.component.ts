d9b353d1f748eb6b8e42479fcf7c08adbc9738c9
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 22:34:58 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,30 @@ import { from } from 'rxjs';
 import { delay, tap } from 'rxjs/operators';
 import { AppService } from './app.service';
 
-// ? From Operatörü belirlenin içini yayınlar.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   myString = 'Deneme';
 
   mySubscribe;
-  constructor(
-    private appService: AppService
-  ) {
-    from(this.myString).pipe(delay(2000), tap(data => {
-      console.log(data);
-    }))
-      .subscribe(data => {
-      console.log(data);
-    })
-
-    this.appService.myAjax.subscribe(data => {
-      console.log(data);
+  constructor(private appService: AppService) {
+    from(this.myString)
+      .pipe(
+        delay(2000),
+        tap((data) => {
+          console.log(data);
+        })
+      )
+      .subscribe((data) => {
+        console.log(data);
       });
+
+    this.appService.myAjax.subscribe((data) => {
+      console.log(data);
+    });
   }
 
   stopSubscribe(): void {
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
 import { from } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { delay, map, mapTo, mergeAll, mergeMap, toArray } from 'rxjs/operators';
+import { delay, mergeMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
+  myAjax = ajax.getJSON('https://jsonplaceholder.typicode.com/users').pipe(
+    mergeMap((mm: any) => (mm = from(mm))),
+    delay(1000)
+  );
 
-  myAjax = ajax.getJSON('https://jsonplaceholder.typicode.com/users')
-    .pipe(
-      mergeMap((mm: any) => mm = from(mm)),
-      delay(1000)
-    );
-
-constructor() { }
-
+  constructor() {}
 }
