7177aa54601732e92014d743d04d8c733712d944
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-31 00:23:36 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  startWith,
-} from 'rxjs/operators';
+import { debounceTime, map } from 'rxjs/operators';
 import { AppService } from './app.service';
 
 @Component({
@@ -16,14 +11,14 @@ import { AppService } from './app.service';
 export class AppComponent implements AfterViewInit {
   @ViewChild('myHtmlInput') myHtmlInput: ElementRef;
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService) {}
 
-  // ? DebounceTime Operatör bu fonksiyona örnek vermek gerekirse klavyede seri tusa basarken zaman limiti koyar ve son veriyi gönderir.
   ngAfterViewInit(): void {
-    fromEvent<any>(this.myHtmlInput.nativeElement, 'keyup').pipe(
-      map((event) => event.target.value),
-      debounceTime(400),
-    ).subscribe(data => console.log(data));
+    fromEvent<any>(this.myHtmlInput.nativeElement, 'keyup')
+      .pipe(
+        map((event) => event.target.value),
+        debounceTime(400)
+      )
+      .subscribe((data) => console.log(data));
   }
-
 }
