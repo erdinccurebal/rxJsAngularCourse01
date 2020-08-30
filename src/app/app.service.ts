d1f277072cbd95d8c5f69ec22b79f77f0445d17f
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 03:35:21 +0300

    2nd commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { distinct, mergeMap, single, toArray } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-constructor() { }
+  myFrom = from([5, 6, 7, 8, 9, 1, 1, 4, 6, 7, 3, 2]);
 
+constructor() { }
+  // ? Distinct
+  findAll(): Observable<any> {
+    return this.myFrom
+      .pipe(
+        distinct(),
+        toArray()
+      );
+  }
 }
