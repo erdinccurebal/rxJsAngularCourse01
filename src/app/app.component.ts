9c77b42362437d7cd6e7469d770ced5661323e62
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-31 00:07:48 +0300

    5th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 
-// ? First Operatörü hata varsa geriye hata mesajı döner, tek obje geri getirir, net aramalar yapılır, tek harfden arama yapılmaz.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-
-  constructor(
-    private appService: AppService
-  ) {
-    this.appService.myFnAjax().subscribe(data => {
+  constructor(private appService: AppService) {
+    this.appService.myFnAjax().subscribe((data) => {
       console.log(data);
     });
   }
