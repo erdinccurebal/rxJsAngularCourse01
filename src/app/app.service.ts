62fc8dbf7a0896fdda96a9fcc44997e81bc73239
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 03:30:56 +0300

    2nd commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { mergeMap, single } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,14 @@ import { Injectable } from '@angular/core';
 export class AppService {
 
 constructor() { }
+
+  // ? Single Operatörü sorgulanan tek bir obje çeker
+  findAll(): Observable<any> {
+    return ajax.getJSON('https://jsonplaceholder.typicode.com/users')
+      .pipe(
+        mergeMap((data: any) => from(data)),
+        single(data => data.username === 'Kamren')
+    )
+  }
 
 }
