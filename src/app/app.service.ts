341dea6a1b57774050b0e141d58e0386581d1380
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 04:02:07 +0300

    2nd commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { mergeMap, skip } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
+  constructor() {}
 
-constructor() { }
-
+  // ? Skip Operator almış olduğu kadar atlama gerçekleştirir.
+  listAllUser(): Observable<any> {
+    return ajax.getJSON('https://jsonplaceholder.typicode.com/users').pipe(
+      mergeMap((data: any) => from(data)),
+      skip(5)
+    );
+  }
 }
