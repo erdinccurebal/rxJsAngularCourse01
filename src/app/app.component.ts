ced0e0624ff41f90415d0d8891d7a823e6ce2f92
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-30 19:35:10 +0300

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { from } from 'rxjs';
+import { skipWhile } from 'rxjs/operators';
+import {} from 'rxjs/ajax';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private appService: AppService
-  ) {
+  constructor(private appService: AppService) {
 
+    // ? SkipWhile Operatörü şarta bağlı olarak atlama yapar.
+    from([2, 3, 4, 6, 8, 3, 13123, 133131, 124])
+      .pipe(skipWhile((value, index) => value < 5))
+      .subscribe((data) => console.log(data));
   }
 }
