221f88044155db73debf291deb8aeac814827823
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-08-31 00:35:20 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent {
   myFrom = from([2, 44, 64, 75, 86, 95, 123, 432, 4564]);
 
   constructor(private appService: AppService) {
-    // ? Parametre olarak int değer alır ve geriye int değer kadar obje çeker. Sondan Başa düzeninde verileri çeker.
     this.myFrom.pipe(takeLast(5)).subscribe((data) => console.log(data));
   }
 }
