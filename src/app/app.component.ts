e42cb3a965d2d266e0e40c499b01e391ccc29e25
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-09-01 02:41:28 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,12 @@ import { tap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private appService: AppService
-  ) {
-
-    range(1, 20).pipe(tap(data => console.log('Gelen Data: ' + data))).subscribe(data => console.log(data));
-
+  constructor(private appService: AppService) {
+    range(1, 20)
+      .pipe(tap((data) => console.log('Gelen Data: ' + data)))
+      .subscribe((data) => console.log(data));
   }
 }
