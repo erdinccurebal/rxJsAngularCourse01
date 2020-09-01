aafad373abeb175c734322a8286245feb4e5764d
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-09-01 16:35:33 +0300

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { of, throwError } from 'rxjs';
+import { timeout, delay, catchError } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private appService: AppService
-  ) {
-    
-
+  constructor(private appService: AppService) {
+    ajax
+      .getJSON('https://jsonplaceholder.typicode.com/users')
+      .pipe(
+        delay(3000),
+        timeout(2000),
+        catchError((err) => throwError(err))
+      )
+      .subscribe((data) => console.log(data));
   }
 }
